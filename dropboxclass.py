6f1db85f819066122a928b1fbddf07af096c987f
sprakasan <76974224+sprakasan@users.noreply.github.com>
2022-01-17 18:59:13 -0500

    Add files via upload

---
+++ dropboxclass.py
@@ -0,0 +1,12 @@
+import cv2
+
+def take_snapshot():
+  videoCaptureObject = cv2.VideoCapture(0)
+  result=True
+  while(result):
+    ret,frame=videoCaptureObject.read()
+    cv2.imwrite("picture1.jpg",frame)
+    result=False
+  videoCaptureObject.release()
+  cv2.destroyAllWindows()
+take_snapshot()
